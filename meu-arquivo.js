772f4f04e0420534062de0f48d550b083092ab0d
MasterKelwin <mkelwin@outlook.com>
2021-09-15 13:51:11 -0300

    1.0

--- meu-arquivo.js
+++ meu-arquivo.js
@@ -4,23 +4,24 @@ function ConverterDolar() {
     var valor = valorElementoDolar.value;
     var valorEmDolarNumerico = parseFloat(valor);
     var valorEmDolar = valorEmDolarNumerico / 5;
+    var valorFinalDolar = valorEmDolar.toFixed(2)
     var elementoValorConvertido = document.getElementById("valorConvertido");
-    var valorConvertidoDolar = "$ " + valorEmDolar;
+    var valorConvertidoDolar = "$ " + valorFinalDolar;
     elementoValorConvertido.innerHTML = valorConvertidoDolar;
   
     //conversão para Bitcoin
     var valorElementoBitcoin = parseFloat(document.getElementById("valor").value);
     var valorEmBitcoin = valorElementoBitcoin / 244000;
+    var valorFinalBTC = valorEmBitcoin.toFixed(6)
     var elementoValorConvertidoBitcoin = document.getElementById("valorConvertidoBitcoin");
-    var valorConvertidoBitcoin = "BTC " + valorEmBitcoin;
+    var valorConvertidoBitcoin = "BTC " + valorFinalBTC;
     elementoValorConvertidoBitcoin.innerHTML = valorConvertidoBitcoin;
   }
   
   //conversão para Euro
   function ConverterEuro() {
-    //conversão para Euro
     var valorElementoEuro = parseFloat(document.getElementById("valor").value);
-    var valorEmEuro = valorElementoEuro / 6;
+    var valorEmEuro = (valorElementoEuro / 6).toFixed(2);
     var elementoValorConvertidoEuro = document.getElementById("valorConvertido");
     var valorConvertidoEuro = "€ " + valorEmEuro;
     elementoValorConvertidoEuro.innerHTML = valorConvertidoEuro;
@@ -28,8 +29,9 @@ function ConverterDolar() {
     //conversão para Bitcoin
     var valorElementoBitcoin = parseFloat(document.getElementById("valor").value);
     var valorEmBitcoin = valorElementoBitcoin / 244000;
+    var valorFinalBTC = valorEmBitcoin.toFixed(6)
     var elementoValorConvertidoBitcoin = document.getElementById("valorConvertidoBitcoin");
-    var valorConvertidoBitcoin = "BTC " + valorEmBitcoin;
+    var valorConvertidoBitcoin = "BTC " + valorFinalBTC;
     elementoValorConvertidoBitcoin.innerHTML = valorConvertidoBitcoin;
   }
   
